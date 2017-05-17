b5dc3aa52734aab3c1ebb265b24577e55a722bab
danielo515 <rdanielo@gmail.com>
2017-05-17 16:45:13 +0200

    - Changed structure to make index simpler (just the entry point) - Added publish config to package.json - Reduced Loger boilerplate - Added tracer API - Added tracer to the Logger API - Added tracer example usage - Added safe defaults to gelfFormatter

---
+++ examples/Traces.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const Logger = require( '../' )( 'TracingExample' );
+
+const trace = Logger.trace('Trace title');
+trace.info( 'This is the first message' );
+trace.log( 'Second message', 'This message also includes some detail' )
+trace.info( 'Third message', { text: 'Complex details can also be includes, like objects' }, [ 'Or arrays' ], [ { cool: ' or maybe also' }, { cool: ' arrays of' }, { cool: 'Objects' }] );
+trace.info( 'Fourth message', 'Using the info method will NOT make the trace go back to that severity level. This is one way road' );
+trace.warn( 'Fifth message', 'We reached the warning level, the entire trace will be flagged as warning' );
+
+trace.flush(); // Print everything to the console!!!
